c4488dd2e324ba126fcbce7740af18371895bb5a
Ella Larson <143456994+ellalarson@users.noreply.github.com>
2023-11-16 17:34:11 -0700

    Update EllaLarson.js

--- src/pages/EllaLarson.js
+++ src/pages/EllaLarson.js
@@ -14,7 +14,7 @@ export default function examplePage() {
             alt='ella larson'
           />
 
-          <p className='description'>
+          <p className='description', style="font-type: georgia">
             I'm from California and have 3 dogs, Frankie, Harley, and Lucy. I love to spend my time 
             watching Greys Anatomy or hanging out with my friends. I wouldn't describe myself as super 
             social, but my roomate does tell me I'm really chatty and always have something to talk 
@@ -62,7 +62,6 @@ export default function examplePage() {
           .description {
             line-height: 1.5;
             font-size: 1.5rem;
-            font-type: georgia;
           }
 
           .pic {
